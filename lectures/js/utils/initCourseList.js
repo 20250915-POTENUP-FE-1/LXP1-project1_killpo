f499ad6fc071825f844c834b6405b798efd932f4
이서정 <need.green.jei@gmail.com>
2025-10-15 11:54:10 +0900

    feat : 과정 카드 (CourseCard) 컴포넌트 수정

--- lectures/js/utils/initCourseList.js
+++ lectures/js/utils/initCourseList.js
@@ -1,11 +1,8 @@
-import { courseList } from "../../js/mockData.js";
+import { store } from "../../../common/js/store/localStorage.js";
 
-export const store = {
-  // mockData를 localStorage에 초기화하는 함수
-  initCourseList() {
-    // localStorage에 courseList가 없으면 저장
-    if (!store.getLocalStorage("courseList")) {
-      store.setLocalStorage("courseList", courseList);
-    }
-  },
+export const initCourseList = () => {
+  // localStorage에 courseList가 없으면 저장
+  if (!store.getLocalStorage("courseList")) {
+    store.setLocalStorage("courseList", courseList);
+  }
 };
