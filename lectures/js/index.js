30052a15fcbc1d5b28f7a2ecaea40d17220ea08b
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-10-16 15:48:12 +0900

    feat: lectures/index.js(lectures 페이지 컨트롤러) 수정
    - 초기화 및 필터링/페이지네이션 통합 로직 적용
    - FilterSidebar, Pagination, renderCourseList 간 데이터 흐름 연결
    	- onFilterChange() → 필터링된 결과를 기반으로 Pagination 상태 갱신
            - onPageChange() → 선택된 페이지에 맞는 강의 리스트 렌더링

--- lectures/js/index.js
+++ lectures/js/index.js
@@ -1,21 +1,35 @@
 import { store } from "../../common/js/store/localStorage.js";
 import { FilterSidebar } from "./components/FilterSidebar.js";
+import { START_PAGE } from "./constants/pagination.js";
 import { renderCourseList } from "./containers/renderCourseList.js";
 import { courseList as mockCourseList } from "./mockData.js";
+import { Pagination } from "./Pagination.js";
+import { filterCourseList } from "./utils/filterCourseList.js";
 
 // 초기 상태 (강좌 리스트, 필터링, 페이지 정보)
 const courseList = store.getLocalStorage("courseList") || mockCourseList; // []
 const filter = { category: [], level: [] };
+const pageNumber = START_PAGE;
+
+// Pagination 초기화
+const pagination = new Pagination({
+  onPageChange: (pageNumber) => {
+    renderCourseList(courseList, filter, pageNumber);
+  },
+});
+pagination.init();
 
 // FilterSidebar 초기화
 const filterSidebar = new FilterSidebar({
   onFilterChange: (filter) => {
-    renderCourseList(courseList, filter);
+    // 페이지네이션 렌더링
+    const filteredCourseList = filterCourseList(courseList, filter);
+    pagination.updateCourseList(filteredCourseList);
+
+    renderCourseList(courseList, filter, pageNumber);
   },
 });
 filterSidebar.init();
 
-// Pagination 초기화
-
 // 최초 강의 리스트 렌더링
-renderCourseList(courseList, filter);
+renderCourseList(courseList, filter, pageNumber);
