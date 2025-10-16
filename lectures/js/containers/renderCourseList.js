8c1209162e6f035f6f4f70a9309911c9071255ec
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-10-16 15:47:52 +0900

    feat: renderCourseList.js 수정
    - 현재 페이지 번호와 필터 정보를 기반으로 CourseCard 리스트 렌더링

--- lectures/js/containers/renderCourseList.js
+++ lectures/js/containers/renderCourseList.js
@@ -1,26 +1,31 @@
 import { courseCard } from "../components/CourseCard.js";
+import { COURSE_PAGE_SIZE } from "../constants/pagination.js";
 import { filterCourseList } from "../utils/filterCourseList.js";
 
 /**
- * @description 필터 정보를 기반으로 강의 리스트를 렌더링한다. (추후에 페이지네이션 관련 정보 추가)
+ * @description 필터, 페이지네이션 정보를 기반으로 강의 리스트를 렌더링한다.
  * @param {Array<Object>} courseList 전체 강의 목록
  * @param {Object} filter 필터 조건 { category: string[], level: string[] }
+ * @param {number} pageNumber 현재 페이지 번호
  */
-export function renderCourseList(courseList, filter) {
+export function renderCourseList(courseList, filter, pageNumber) {
   const container = document.querySelector(".course-grid");
   container.innerHTML = "";
 
   // 필터링
   const filteredCourseList = filterCourseList(courseList, filter);
 
-  // 페이지 분리
+  // 페이지네이션
+  const startIndex = (pageNumber - 1) * COURSE_PAGE_SIZE;
+  const paginatedCourseList = filteredCourseList.slice(
+    startIndex,
+    startIndex + COURSE_PAGE_SIZE
+  );
 
   // 강의 리스트 렌더링
-  if (!filteredCourseList.length) {
+  if (!paginatedCourseList.length) {
     container.innerHTML = `<p class="course-card-empty-message">조건에 맞는 강의가 없습니다.</p>`;
   } else {
-    container.innerHTML = filteredCourseList.map(courseCard).join("");
+    container.innerHTML = paginatedCourseList.map(courseCard).join("");
   }
-
-  // 페이지네이션
 }
