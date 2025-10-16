88f39cac45f4811abb0a11b9963b394a33462897
이서정 <need.green.jei@gmail.com>
2025-10-16 19:40:26 +0900

    feat: 공통 모달 타이틀·서브타이틀 변경 기능 구현 수정 1) bindModalEvents 내부에 updateModalText 함수 코드 이동

--- common/js/utils/bindModalEvents.js
+++ common/js/utils/bindModalEvents.js
@@ -1,5 +1,5 @@
 import { $ } from "../utils/dom.js";
-import { updateModalText } from "./updateModalText.js";
+import { modalTexts } from "../constants/modalTexts.js";
 
 export function bindModalEvents() {
   $(".container").addEventListener("click", (e) => {
@@ -35,4 +35,26 @@ export function bindModalEvents() {
 
 function closeModal(modal) {
   modal.style.display = "none";
+}
+
+export function updateModalText(modal, mode) {
+  const modalId = modal.id;
+  // 모달 id에 텍스트 객체 가져오기
+  const textConfig = modalTexts[modalId];
+
+  // 모달 mode 를 확인
+  if (!textConfig || !textConfig[mode]) return;
+
+  //현재 모드에 맞는 텍스트 가져오기
+  const config = textConfig[mode];
+
+  // 모달 내부에서 상단작은제목, 제목, 서브타이틀 선택
+  const eyebrowElement = modal.querySelector(".modal__eyebrow");
+  const titleElement = modal.querySelector(".modal__title");
+  const subtitleElement = modal.querySelector(".modal__subtitle");
+
+  // 요소가 있다면 텍스트 불러오기
+  if (eyebrowElement) eyebrowElement.textContent = config.eyebrow;
+  if (titleElement) titleElement.textContent = config.title;
+  if (subtitleElement) subtitleElement.textContent = config.subtitle;
 }
