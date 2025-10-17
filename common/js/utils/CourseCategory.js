3b5d8847b660b11d50a3c0947336ca699b094d00
이서정 <need.green.jei@gmail.com>
2025-10-17 21:42:30 +0900

    feat: 강의 등록하기(RegisterCourseModal) 구현 수정

--- common/js/utils/CourseCategory.js
+++ common/js/utils/CourseCategory.js
@@ -1,13 +1,13 @@
 import { CATEGORY } from "../../../lectures/js/constants/category.js";
 
 // 1차 카테고리 목록 가져오기
-export function getFirstCategories() {
+export function getFirstCategory() {
   return Object.keys(CATEGORY).filter((key) => key !== "전체");
   // 전체는 카테고리가 아니여서 제외 처리
 }
 
 // 1차에서 선택한 카테고리 받아와 2차 카테고리 목록 가져오기 , 값이 없으면 반환
-export function getSecondCategories(firstCategory) {
+export function getSecondCategory(firstCategory) {
   if (!firstCategory || !CATEGORY[firstCategory]) {
     return [];
   }
@@ -15,7 +15,7 @@ export function getSecondCategories(firstCategory) {
 }
 
 // 1차, 2차가 완료되면 3차 카테고리 목록 가져오기
-export function getThirdCategories(firstCategory, secondCategory) {
+export function getThirdCategory(firstCategory, secondCategory) {
   if (
     !firstCategory ||
     !secondCategory ||
--- common/js/utils/bindCategorySelect.js
+++ common/js/utils/bindCategorySelect.js
@@ -1,7 +1,7 @@
 import {
-  getFirstCategories,
-  getSecondCategories,
-  getThirdCategories,
+  getFirstCategory,
+  getSecondCategory,
+  getThirdCategory,
 } from "./CourseCategory.js";
 import { $ } from "./dom.js";
 
@@ -13,8 +13,8 @@ export function bindCategorySelect() {
 
   // 1차 카테고리 옵션 생성
   if (primarySelect) {
-    const firstCategories = getFirstCategories();
-    const options = firstCategories
+    const firstCategory = getFirstCategory();
+    const options = firstCategory
       .map((cate) => `<option value="${cate}">${cate}</option>`)
       .join("");
     primarySelect.innerHTML = `<option value="">1차 카테고리 선택</option>${options}`;
@@ -23,9 +23,9 @@ export function bindCategorySelect() {
   // 1차 카테고리 변경 이벤트
   primarySelect?.addEventListener("change", (e) => {
     const selectedFirst = e.target.value;
-    const secondCategories = getSecondCategories(selectedFirst);
+    const secondCategory = getSecondCategory(selectedFirst);
 
-    const options = secondCategories
+    const options = secondCategory
       .map((cate) => `<option value="${cate}">${cate}</option>`)
       .join("");
 
@@ -43,9 +43,9 @@ export function bindCategorySelect() {
   secondarySelect?.addEventListener("change", (e) => {
     const selectedFirst = primarySelect.value;
     const selectedSecond = e.target.value;
-    const thirdCategories = getThirdCategories(selectedFirst, selectedSecond);
+    const thirdCategory = getThirdCategory(selectedFirst, selectedSecond);
 
-    const options = thirdCategories
+    const options = thirdCategory
       .map((cate) => `<option value="${cate}">${cate}</option>`)
       .join("");
 
