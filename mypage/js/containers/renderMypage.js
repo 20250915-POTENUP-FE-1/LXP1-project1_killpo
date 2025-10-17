007b22387437b9b75e7782e51b85828886a5fea4
syunghoon <shcho2109@gmail.com>
2025-10-17 17:30:00 +0900

    feat: MypageComponent 등록한 강의 삭제하기 구현

--- mypage/js/containers/renderMypage.js
+++ mypage/js/containers/renderMypage.js
@@ -1,12 +1,16 @@
-import { $ } from "../../../../common/js/utils/dom.js";
+import { $ } from "../../../common/js/utils/dom.js";
 import { CourseItem } from "../components/CourseItem.js";
+import { deleteCourse } from "../utils/DeleteCourse.js";
 
 /**
  * @param {Array<Object>} courseList 전체 강의 목록
  */
+
 export function renderMypage(courseList) {
+  console.log(courseList);
   // 총 강의 개수 표시
   $(".mypage-content__total").innerHTML = courseList.length;
+
   if (!courseList.length) {
     $(
       ".course-table__body"
@@ -15,27 +19,10 @@ export function renderMypage(courseList) {
     $(".course-table__body").innerHTML = courseList
       .map((courseItem) => CourseItem(courseItem))
       .join("");
+    bindEvents();
   }
-  // 테이블에 이벤트 핸들러 위임
-  bindEvents();
 }
+
 const bindEvents = () => {
-  $(".course-table__body").addEventListener("click", (e) => {
-    const target = e.target;
-    // 삭제하기 버튼 클릭 이벤트 등록
-    if (target.classList.contains("delete-course-btn")) {
-      // 삭제하기 로직 구현 후 유틸 함수로 분리 예정
-      const courseRow = target.closest(".course-row");
-      const courseId = courseRow.dataset.courseId;
-      console.log(courseId);
-      alert(`${courseId} 강의를 삭제하시겠어요? 삭제 후 복구가 불가능해요 🥲`);
-    }
-    // 삭제하기 버튼 클릭 이벤트 등록
-    if (target.classList.contains("edit-course-btn")) {
-      const courseRow = target.closest(".course-row");
-      const courseId = courseRow.dataset.courseId;
-      console.log(courseId);
-      alert(`${courseId} 강의를 수정하시겠어요?`);
-    }
-  });
+  $(".course-table__body").addEventListener("click", deleteCourse);
 };
---
+++ mypage/js/utils/deleteCourse.js
@@ -0,0 +1,29 @@
+import { store } from "../../../common/js/store/localStorage.js";
+import { courseList as mockCourseList } from "../../../lectures/js/mockData.js";
+import { CourseItem } from "../components/CourseItem.js";
+
+export const deleteCourse = (e) => {
+  const target = e.target;
+  if (target.classList.contains("delete-course-btn")) {
+    // 삭제하기 로직 구현 후 유틸 함수로 분리 예정
+    const courseRow = target.closest(".course-row");
+    const selectedId = courseRow.dataset.courseId;
+
+    // 확인 버튼 클릭 시 삭제
+    if (
+      confirm(
+        `${selectedId} 강의를 삭제하시겠어요? 삭제 후 복구가 불가능해요 🥲`
+      )
+    ) {
+      // 강의 목록 업데이트
+      const courseList = store.getLocalStorage("courseList") || mockCourseList; // []
+      const newList = courseList.filter((course) => course.id !== selectedId);
+      store.setLocalStorage("courseList", newList);
+
+      // 강의 목록 렌더링
+      target.closest(".course-table__body").innerHTML = newList
+        .map((courseItem) => CourseItem(courseItem))
+        .join("");
+    }
+  }
+};
