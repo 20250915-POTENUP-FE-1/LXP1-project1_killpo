f4fb6d5c1dfc30da675983165830937c3b6938c6
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-10-19 23:54:40 +0900

    fix: CourseCard 오버레이 레이아웃 수정 → 텍스트가 영역 밖으로 나가지 않도록 처리

--- lectures/js/components/CourseCard.js
+++ lectures/js/components/CourseCard.js
@@ -35,17 +35,19 @@ export function CourseCard(courseItem) {
        <!-- 강좌 상세 오버레이 -->
       <div class="course-card__hover-panel">
         <div class="course-card-detail">
-          <div class="course-card-detail__header">
-            <h3 class="course-card-detail__title"> ${courseItem.title} </h3>
-          </div>
-          <p class="course-card-detail__description">
-          ${courseItem.description}              
-          </p>
-          <div class="course-card-tags">
-            <ul> ${courseItem.tags
-              .map((tag) => `<li id="course-card-tag">${tag}</li>`)
-              .join("")}
-            </ul>
+          <div class="course-card-detail__container">
+            <div class="course-card-detail__header">
+              <h3 class="course-card-detail__title"> ${courseItem.title} </h3>
+              <p class="course-card-detail__description">${
+                courseItem.description
+              }</p>
+            </div>
+            <div class="course-card-detail__tags">
+              <ul> ${courseItem.tags
+                .map((tag) => `<li id="course-card-tag">${tag}</li>`)
+                .join("")}
+              </ul>
+            </div>
           </div>
           <div class="course-card-detail__actions">
              <a href="${buildUrl(
